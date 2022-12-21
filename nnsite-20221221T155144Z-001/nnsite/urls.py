e95dda1592c9037367882f14043dba83aada3a96
naufalnass <naufaln176@gmail.com>
2022-12-22 00:06:26 +0700

    Delete nnsite-20221221T155144Z-001/nnsite directory

--- nnsite-20221221T155144Z-001/nnsite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
-    path("todolist/", include("todolist.urls")),
-    path("", include("todolist.urls")),
-]
